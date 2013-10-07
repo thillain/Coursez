9d00eb04ec585cf0a141a9011fb0c9f1cfeb4a33
JudeArasu <judearasu@gmail.com>
2013-10-07 23:02:44 +0530

    jquery course added

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class Main{
+	/**
+	hashset: Set has no duplicates
+	*/
+	public static void main(String[] args)
+	{
+		HashSet<Integer> phones = new HashSet<Integer>();
+		phones.add(45566);
+		System.out.println(phones);
+	}
+}
