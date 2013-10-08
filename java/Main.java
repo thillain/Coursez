51fd6193988a4ef3f331f0255f3b998add24bf05
JudeArasu <judearasu@gmail.com>
2013-10-08 05:48:46 +0530

    included hashmap

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,10 @@
 import java.util.HashSet;
+import java.util.HashMap;
 
 public class Main{
 	/**
-	hashset: Set has no duplicates
+	*hashset: Set has no duplicates
+    *hashmap: Stores key-value pairs
 	*/
 	public static void main(String[] args)
 	{
@@ -10,7 +12,13 @@ public class Main{
 		phones.add(45566);
 		phones.add(45566);
 		phones.add(55566);
-
 		System.out.println(phones);
-	}
+
+		HashMap<String, Integer> directory = new HashMap<String,Integer>();
+		directory.put("v51gtm",3456);
+		directory.put("larry",3456);
+		directory.put("queen",3456);
+		System.out.println(directory);
+		System.out.println(directory.get("larry"));
+ 	}
 }
