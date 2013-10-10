0d57cae5d062bc5ec87be7d6fdfe43b34eb0d778
JudeArasu <judearasu@gmail.com>
2013-10-10 06:59:59 +0530

    ng-json still not works

--- angular/ng-json/scripts/controllers/show.js
+++ angular/ng-json/scripts/controllers/show.js
@@ -1,10 +1,27 @@
+
 app.controller("jsoncontroller",function($scope,$http){
-	//API requires a start date
-	var today = new Date();
-	$http.jsonp('//builds.apache.org/api/json?pretty=true').success(function(data) {
-		var log = [];
-		angular.forEach(values, function(value, key){
-			this.push(key + ': ' + value);
-		}, log);
-	});
+ 
+ 	//API requires a start date
+	//var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+  var url='https://builds.apache.org/api/json?format=json';
+  //url='http://ip.jsontest.com/'
+  $http.jsonp(url+'?callback=JSON_CALLBACK').success(function(data){
+       console.log('success');
+    })
+    .error(function () {
+      console.log('error')
+    });
+   // var url = "https://builds.apache.org/api/json";
+    //CORS support
+    //delete $http.defaults.headers.common['X-Requested-With'];
+
+    //$http.jsonp(url).success(function(data) {
+    //	console.log(data);
+    //});
+
 });
+
+
+//function spotify_api($http) {
+
+//})
