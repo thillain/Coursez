e7cb1107d07adec41d6d60535f21186aa62bab68
JudeArasu <judearasu@gmail.com>
2013-10-10 23:30:33 +0530

    cross origin script example

--- angular/ng-json/scripts/controllers/show.js
+++ angular/ng-json/scripts/controllers/show.js
@@ -3,7 +3,7 @@ app.controller("jsoncontroller",function($scope,$http){
  
  	//API requires a start date
 	//var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
-  var url='https://builds.apache.org/api/json?format=json';
+  var url='https://builds.apache.org/api/json';
   //url='http://ip.jsontest.com/'
   $http.jsonp(url+'?callback=JSON_CALLBACK').success(function(data){
        console.log('success');
@@ -11,14 +11,6 @@ app.controller("jsoncontroller",function($scope,$http){
     .error(function () {
       console.log('error')
     });
-   // var url = "https://builds.apache.org/api/json";
-    //CORS support
-    //delete $http.defaults.headers.common['X-Requested-With'];
-
-    //$http.jsonp(url).success(function(data) {
-    //	console.log(data);
-    //});
-
 });
 
 
