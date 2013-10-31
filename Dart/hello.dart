339d6c4b1fbf2f99e4ef7c8b3aae93f8fcbb3c89
JudeArasu <judearasu@gmail.com>
2013-10-31 08:23:23 +0530

    polymer added

--- Dart/hello.dart
+++ Dart/hello.dart
@@ -1,10 +1,19 @@
 // Define a function
 printNumber(num aNumber)
 {
-  print('The number is $aNumber.');
+	print('The number is $aNumber.');
+	//Ex 2
+	var one = int.parse('1');
+	print(one);
+	//Ex 3
+	var list = [1,2,3];
+	assert(list.length == 3);
+	print(list.length);
 }
 main()
 {
-  var number=42;
-  printNumber(number);
+	var number=42;
+	printNumber(number);
 }
+
+
