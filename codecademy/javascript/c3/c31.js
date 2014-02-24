f2bdddb84f8c0757e3a9624e0b8d4e718b960387
JudeArasu <judearasu@gmail.com>
2014-02-24 23:10:53 +0530

    functions tutorial

---
+++ codecademy/javascript/c3/c31.js
@@ -0,0 +1,12 @@
+var speed = 100;
+
+// Complete the condition in the ()s on line 4
+if (speed < 80) {
+	// Use console.log() to print "Slow down
+	console.log("Slow down")
+} 
+else {
+	// Use console.log() to print "Drive safe"
+console.log("Drive safe")
+
+}
---
+++ codecademy/javascript/c3/c32.js
@@ -0,0 +1,12 @@
+// This is what a function looks like:
+
+var divideByThree = function (number) {
+    var val = number / 3;
+    console.log(val);
+};
+
+// On line 12, we call the function by name
+// Here, it is called 'dividebythree'
+// We tell the computer what the number input is (i.e. 6)
+// The computer then runs the code inside the function!
+divideByThree(6);
