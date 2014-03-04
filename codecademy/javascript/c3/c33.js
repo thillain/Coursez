67df599cce0278a98d4ea488d87127b81e6aedd0
JudeArasu <judearasu@gmail.com>
2014-03-04 22:16:14 +0530

    simple fn

---
+++ codecademy/javascript/c3/c33.js
@@ -0,0 +1,11 @@
+// Below is the greeting function!
+// See line 7
+// We can join strings together using the plus sign (+)
+// See the hint for more details about how this works.
+
+var greeting = function (name) {
+    console.log("Great to see you," + " " + name);
+};
+
+greeting("ThillaiArasu")
+// On line 11, call the greeting function!
---
+++ codecademy/javascript/c3/c34.js
@@ -0,0 +1,5 @@
+//Simple function
+var foodDemand = function(food){
+    console.log("I want to eat" + " " + food);
+}
+foodDemand("Dosa");
