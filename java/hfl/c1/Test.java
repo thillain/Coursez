34362384c4a696c64a07dedcb2f198ca3fcf4707
JudeArasu <judearasu@gmail.com>
2014-10-24 08:02:18 -0400

    Puzzle test java file

---
+++ java/hfl/c1/Test.java
@@ -0,0 +1,11 @@
+class Test{
+public static void main(String [] args){
+int x=0;
+int y=0;
+while(x<5){
+y = y+2;
+System.out.print(x + "" + y + "");
+x=x+1;
+}
+}
+}
---
+++ java/hfl/c1/party.java
@@ -0,0 +1,14 @@
+#Simple Compiler Java
+import java.awt.*;
+import java.awt.event.*;
+
+class Party{
+public void buildInvite(){
+Frame f = new Frame();
+Label l = new Label("Party at Tims");
+Button b = new Button("You be");
+Button c = new Button("Shoot me");
+Panel p = new Panel();
+p.add(I);
+}
+}
