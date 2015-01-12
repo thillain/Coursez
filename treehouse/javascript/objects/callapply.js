fcdf912877addaa53e5f0c4a74dbfcd73895bb58
JudeArasu <judearasu@gmail.com>
2015-01-12 10:24:01 -0500

    native js

---
+++ treehouse/javascript/objects/callapply.js
@@ -0,0 +1,24 @@
+// JavaScript Objects
+
+var jim={
+	name:"Jim",
+	skills: ["JavaScript","Ruby","Dancing"],
+	"favcolor": "green",
+	greet: function(){
+		console.log("Hello i am "+ this.name);
+
+	}
+};
+
+var nick={
+	name:"Nick",
+	greet: jim.greet
+};
+
+jim.name="Thanuja";
+
+jim["greet"]();
+nick.greet();
+
+var jimGreet=jim.greet;
+jimGreet.call({name:"Arun"});
